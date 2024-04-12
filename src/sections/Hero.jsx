9df2b5c7fb74ed8c701e071940155c1543fdc539
Alireza KafshdarToosi <a.k.toosi@gmail.com>
2024-04-12 16:50:03 +0200

    added hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,7 +1,11 @@
 export default function Hero() {
   return (
     <section className="hero">
-      <h1>i'm Alireza</h1>
+      <h1>Welcome to Alireza's portfolio</h1>
+      <span className="subtitle">
+      Crafting Digital Solutions with Passion and Precision
+      </span>
+
     </section>
   );
 }
