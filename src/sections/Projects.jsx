254f83336c771a902317eab07b98239146778a84
Alireza KafshdarToosi <a.k.toosi@gmail.com>
2024-04-13 11:29:38 +0200

    Added Project data, code and css

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,8 +1,20 @@
+import Data from "../data/projects.json";
+
 export default function Projects() {
+  const items = Data.map((item) => (
+    <article className="item-project" key={item.id}>
+      <img src={item.image_thumb_url} />
+      <h3>{item.description}</h3>
+    </article>
+  ));
   return (
     <section className="projects">
       <div className="container">
         <h2>Projects</h2>
+        <div className="content-grid">
+          <p>Here are my projects .</p>
+          <div className="items-grid">{Items}</div>
+        </div>
       </div>
     </section>
   );
